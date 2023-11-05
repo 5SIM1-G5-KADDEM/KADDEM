8f4c59c01e5d0f6b3f6d4feed3492bcb59baa93b
mohamedjasserbensmida <74205089+mohamedjasserbensmida@users.noreply.github.com>
2023-11-05 15:50:28 +0100

    Graffama&Prometheus

--- src/main/java/tn/esprit/spring/kaddem/KaddemApplication.java
+++ src/main/java/tn/esprit/spring/kaddem/KaddemApplication.java
@@ -3,10 +3,14 @@ package tn.esprit.spring.kaddem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableScheduling
 @SpringBootApplication
 public class KaddemApplication {
+
+
 
     public static void main(String[] args) {
         SpringApplication.run(KaddemApplication.class, args);
